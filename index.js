04e34dd4f424b221bb70fe6992160bae77c19629
Christian Vincent <christianjvincent318@gmail.com>
2022-01-13 20:47:30 -0500

    Initial Project Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+(function () {
+  const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+window.toTitleCase = toTitleCase;
+function handleUserSearch (){
+  const inputValue = document.getElementById("search-bar").value;
+  const inputValue2 = inputValue.replace(" ", "%20");
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=6e5552cf92372341dd989c6f8d82a257&language=en-US&query=${inputValue2}&page=1&include_adult=false`, {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(response => {
+    renderPosters(response);
+  })
+  .catch(err => {
+    
+  });
+  const renderPosters = (response) => {
+    console.log(response);
+  }
+}
+document.getElementById("button").onclick = function() {
+  handleUserSearch();
+}
+})();
